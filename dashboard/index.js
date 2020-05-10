5be951370c8b2cccf759889dd99da14be1e6e3c0
Rich Hart <richard.hart@capco.com>
2020-05-10 00:10:18 -0400

    add proxy

--- dashboard/index.js
+++ dashboard/index.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const { createProxyMiddleware} = require('http-proxy-middleware');
+
 const app = express();
 
 app.use(express.static('public'))
 //app.use(express.static('./node_modules/chart.js/dist/'))
 app.use(express.static('./node_modules/lcars-sdk'))
 app.use(express.static('./node_modules/particles'))
+
+
+app.use(
+   '/api',
+   createProxyMiddleware(
+      {target: 'http://127.0.0.1:8001/', changeOrigin:true})
+)
 
 //app.get('/', (req, res) => {
 //  res.send('Hello World!')
